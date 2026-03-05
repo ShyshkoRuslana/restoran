084c374691b9dd4bff2e5e1238bd1289f07c3b90
Ruslana <shyshkoruslana08@gmail.com>
2026-03-05 23:29:47 +0200

    changed the data in Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,7 +1,7 @@
 #include "Customer.h"
 #include <iostream>
 Customer::Customer(string name, int tablenumber, int telnumber): name(name), tablenumber(tablenumber), telnumber(telnumber){}
-Customer::Customer(): Customer("Guest", 1, 234) {}
+Customer::Customer(int tablenamber): Customer("Guest", 1, 0) {}
 Customer::~Customer() {
     cout << "Customer destroyed:" << name << endl;
 }
